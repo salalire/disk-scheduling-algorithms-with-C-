c31efb81b16e01784abf9c2e5eabe8a624a38b60
paulos27 <paulosasmelash21@gmail.com>
2026-05-09 20:58:16 +0300

    Added Comparison.h implementation

---
+++ implimentation/Comparison.h
@@ -0,0 +1,11 @@
+// comparison.h
+#ifndef COMPARISON_H
+#define COMPARISON_H
+
+#include <vector>
+#include <string>
+using namespace std;
+
+void compareAlgorithms(vector<int> requests, int head, int disk_size, string direction);
+
+#endif
